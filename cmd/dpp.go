fee29036cd4ecc768f846a8155e0429c1b90fb32
elfredburger <elfredfredburger@gmail.com>
2024-03-07 19:07:49 +0100

    basic update

--- cmd/dpp.go
+++ cmd/dpp.go
@@ -1,19 +1,27 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // dppCmd represents the dpp command
 var dppCmd = &cobra.Command{
-	Use:   "dpp",
-	Short: "A brief description of your command",
+	Aliases: []string{"start"},
+	Use:     "dpp",
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +29,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dpp called")
+
+		fmt.Printf("bot started ", appVersion)
+		dpp, err := telebot.NewBot(telebot.Settings{
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			fmt.Print("rgertgerferfweedd", TeleToken)
+			log.Fatalf("Token issue", err)
+		}
+		dpp.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			return err
+		})
+
+		dpp.Start()
 	},
 }
 
